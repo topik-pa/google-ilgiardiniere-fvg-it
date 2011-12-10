d12bc625f016d4693cdabb089b7fd85ed54c6786
marcopavan.mp@gmail.com <marcopavan.mp@gmail.com@c64b2b40-e330-d85d-576b-b528cfa639bf>
2011-12-10 21:42:15 +0000

    copia dei file di progetto

---
+++ index.php
@@ -0,0 +1,159 @@
+<!-- Home page del sito -->
+<!-- Autore: Marco Pavan - pavan.marco@email.it -->
+<!-- Data: 28 sett 2010 -->
+<?php
+//Imposto due variabili di uso generale
+$titolo_pagina = "Giardiniere a Udine in Friuli - manutenzione verde, realizzazioni giardini e abbattimento alberi";
+$pagina = "home";
+?>
+
+<!-- Dichiarazione del DOCTYPE -->
+<?php include('./includes/doctype.php'); ?>
+
+<!-- Corpo del documento -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it">
+	<head>
+		<!-- Inserimento del tag head -->
+		<?php include('./includes/tag_head.php'); ?>
+	</head>
+	<body>	
+		<!-- Link per raggiungere direttamente il contenuto principale saltando le voci del men&ugrave; -->
+		<p id="vaiAlCorpoCentrale"><a href="#supportingText">Giardiniere a Udine - vai al contenuto principale</a></p>	
+		<!-- Wrapper del contenuto della pagina -->
+		<div id="container">
+			<!-- Introduzione -->
+			<div id="intro"> 
+				<!-- Header della pagina -->					
+				<?php include('./includes/tag_div_pageHeader.php'); ?>
+				<!-- Fine header della pagina -->
+				<!-- Breve riassunto degli argomenti o spazio per avvisi -->				
+				<div id="quickSummary">		
+
+				</div>
+				<!-- Fine riassunto degli argomenti o avvisi -->
+				<!-- Breve preambolo -->
+				<div id="preamble"> 
+
+				</div>				
+				<!-- Fine preamble -->
+				<!-- Menu orizzontale -->			
+				<?php include('./includes/tag_div_menu_orizzontale.php'); ?>			
+				<!-- Fine menu orizzontale -->
+				<!-- Breadcrums della pagina (modifica manuale) e search engine interno-->
+				<div id="breadcrums_searchEngine">
+
+				</div>			
+				<!-- Fine breadcrums_searchEngine -->
+			</div>			
+			<!-- Fine div intro -->
+
+			<!-- Testo principale del documento -->
+			<div id="supportingText">
+				<!--Inclusione della funzione stampa di printFriendly -->	
+				<?php include('./includes/print_friendly.php'); ?>
+				<!-- Fine print friendly -->	
+				<!-- Indice argomenti del corpo centrale -->
+				<div id="indiceCorpoCentrale">
+					<h5>Giardiniere a Udine ed in tutto il Friuli: manutenzione verde, giardini, siepi, prati e piante da giardino. Potatura e abbattimento alberi.</h5>
+					<p>Keywords: <em>giardiniere, udine, friuli, manutenzione, verde, alberi, piante, giardino, prato, siepi, potature, abbattimento, alberi, consulenza, giardinaggio</em></p>
+	  			</div>				
+				<!-- Fine div indiceCorpoCentrale -->
+				<!-- Primo paragrafo -->
+				<div id="explanation">
+					<h3><span>Il nostro giardino: manutenzione del verde</span></h3>
+					<p>Il nostro verde di casa deve essere visto come un luogo dove stare in armonia e in relax, un posto in cui ritrovare la stessa sensazione di comfort e tranquillit&agrave; che proviamo quando siamo all'interno delle nostre pareti domestiche. <br />Il nostro giardino non deve essere uno spazio in pi&ugrave; che origina solo lavoro e grattacapi... Giusto? <br /><br />
+					</p>
+
+					<h3><span>Il mestiere di giardiniere: manutenzione del giardino e cura delle piante</span></h3>
+					<a href="gallery.php?pagina=index&amp;img=1"><img id="foto1" class="classe_immagini" alt="Giardiniere a Udine (in Friuli): lavoro di manutenzione giardino in Friuli" src="img/1ico.jpg" /></a>
+					
+					
+					<p class="p1">
+					<span>Un mestiere, seppur per certi aspetti soggettivo, non si inventa! <br />Eseguire una potatura di un albero non &egrave; facile perch&egrave; &quot;<em>lo si &egrave; letto</em>&quot; o perch&egrave; &quot;<em>&egrave; sempre stato fatto cos&igrave;</em>&quot;. <br />Il mestiere di giardiniere &egrave; la somma di conoscenza tecnica delle piante e professionalit&agrave;. Solo in questo modo si pu&ograve; raggiungere l'obiettivo che ci si &egrave; prefissati.<br />Il lavoro deve essere eseguito con attrezzatura idonea e soprattutto con un attenzione particolare alla sicurezza di chi opera!!!<br />Un bel manto erboso non se lo pu&ograve; ottenere riportando un poco di terra e gettando dei semi (magari i semi presi in offerta al centro commerciale). Per un prato bello e duraturo, bisogna conoscer al meglio le caratteristiche del terreno. Altri aspetti fondamentali sono il momento della semina, le concimazioni mirate e, ovviamente, la scelta pi&ugrave; appropriata della semenza, che varia a seconda di ogni situazione.
+					<a href="gallery.php?pagina=index&amp;img=3"><img id="foto3" class="classe_immagini" alt="Giardiniere a Udine e in Friuli: lavori di manutenzione siepi in comune di Udine" src="img/3ico.jpg" /></a>
+					</span>
+					<br /><br /><br /><br /><br /><br /><br /><br /></p>
+					<h3><span>Come lavoro? Un giardiniere per la progettazione, realizzazione e manutenzione del tuo giardino</span></h3>
+					<a href="gallery.php?pagina=index&amp;img=2"><img id="foto2" class="classe_immagini" alt="Manutenzione di alberi e siepi in comune di Udine - Friuli" src="img/2ico.jpg" /></a>
+					<ul>
+						<li><strong>Opero come giardiniere a Udine ed in tutto il Friuli</li></strong>
+						<li>Rapporto fiducioso e duraturo nel tempo</li>
+						<li>Preparazione tecnica data da studi appropriati e passione verso il tuo giardino e la manutenzione del verde</li>
+						<li>Specializzazione nella lotta contro la processionaria.</li>
+						<li>Macchinari da giardinaggio adeguati allo scopo</li>
+					</ul>
+					<p class="p2">
+
+					<span>I miei servizi comprendono tra l'altro:
+					</span>
+					</p>
+					<ul>
+						<li><em>Giardiniere manutenzione giardini</em></li>
+						<li><em>Giardiniere manutenzione verde</em></li>
+						<li><em>Giardiniere manutenzione prato</em></li>
+						<li><em>Giardiniere manutenzione piante</em></li>
+						<li><em>Giardiniere manutenzione siepi</em></li>
+						<li><em>Giardiniere abbattimento alberi</em></li>
+						<li><em>Giardiniere potatura alberi</em></li>
+						<li><em>Giardiniere progettazione giardini</em></li>
+						<li><em>Giardiniere realizzazione giardini</em></li>
+					</ul>
+					<p>Ed ancora, come giardiniere eseguo a Udine ed in tutto il Friuli:</p>
+					<ul> 
+						<li>Potature conifere e alberi d'alto fusto</li>
+						<li>Raccolta e smaltimento di tutti i residui di potatura</li>
+						<li>Progettazione, creazione e manutenzione aiuole con fornitura piante</li>
+						<li>Posa prato in rotoli</li>
+						<li>Lavorazione terreno e semine </li>
+						<li>Arieggiature e trasemine</li>
+						<li>Creazione recinzioni</li>
+						<li>Eliminazione totale della <a href="./malattie_delle_piante/processionarie/processionaria/processionaria.php" title="Giardiniere a Udine: vai alla pagina sulla processionaria">processionaria</a> e trattamenti endoterapici </li>
+						<li>Individuazione e lotta contro tutti gli attacchi parassitari e microcarenze che si possono trovare sulle piante</li>
+						<li><strong>Preventivi e consulenza gratuiti!!!</strong></li>
+					</ul>
+
+				</div>				
+				<!-- Fine div explanation -->
+				<!-- Secondo paragrafo -->
+				<div id="participation">	
+
+				</div>				
+				<!-- Fine div participation -->
+				<!-- Terzo paragrafo -->
+				<div id="benefits">		
+
+				</div>				
+				<!-- Fine div benefits -->
+				<!-- Quarto paragrafo -->
+				<div id="requirements">		
+					
+				</div>				
+				<!-- Fine div requirements -->
+				
+				<?php include('./includes/tag_div_footer.php'); ?>
+				<!-- Fine footer -->
+			</div>				
+			<!-- Fine div supportingText -->
+			<!-- LinkList (menu di navigazione) -->
+			<div id="linkList">
+				<!-- Menu di navigazione 1 -->
+				<?php include('./includes/tag_div_Navigation1.php'); ?>	
+				<!-- LinkList2 (menu2) -->		
+				<div id="linkList2">	
+							
+				</div>	
+				<!-- Fine div linkList2 (menu2) -->
+			</div>	
+			<!-- Fine div linkList (menu) -->
+		</div>			
+		<!-- Fine div container -->
+		<!-- Sezioni div extra per immagini, toolbox etc...
+		<div id="extraDiv1"><span></span></div><div id="extraDiv2"><span></span></div><div id="extraDiv3"><span></span></div>
+		<div id="extraDiv4"><span></span></div><div id="extraDiv5"><span></span></div><div id="extraDiv6"><span></span></div>
+		-->
+		<!-- Inserimento codice StatCounter -->
+		<?php include('./includes/statCounterCode.php'); ?>
+		<!-- Fine codice StatCounter -->
+	</body>
+</html>
+<!--fine home page -->
