984a9100cb74ddb8b80000bbae79aae7c240556d
marcopavan.mp@gmail.com <marcopavan.mp@gmail.com@c64b2b40-e330-d85d-576b-b528cfa639bf>
2011-12-10 21:45:42 +0000

    copia dei file di progetto 2

---
+++ includes/tag_div_colonnaSx.php
@@ -0,0 +1,61 @@
+<!-- Colonna sinistra del sito -->
+			<!-- Autore: Marco Pavan - pavan.marco@email.it -->
+			<!-- Data: 28 sett 2010 -->
+			<div id="colonnaSx">
+				<!-- Pulsante inserimento nei bookmarks -->
+				<!--<a href="javascript:preferiti()"> Aggiungi ai preferiti</a>-->
+				<br /><br />
+				<div id="menu_verticale_1">		<!--Menu di navigazione 1 in colonna sinistra-->
+					<ul id="lista_menu_verticale_1">
+						<!--<li class="fake_item"></li>-->
+						<li id="home_v"><a accesskey="I" tabindex="0" title="Vai alla home page" href="/index.php">Home</a></li>
+						<li id="chi_siamo_v"><a accesskey="L" tabindex="1" title="Vai alla pagina: Chi siamo" href="/chi_siamo/chi_siamo.php">Chi siamo</a></li>
+						<li id="luogo_v"><a accesskey="M" tabindex="2" title="Vai all pagina: Localit&agrave;" href="/il_luogo/il_luogo.php">La localit&agrave;</a></li>
+						<li id="news_v"><a accesskey="N" tabindex="3" title="Vai all pagina: News" href="/news/news.php">News</a></li>
+						<li id="come_raggiungerci_v"><a accesskey="O" tabindex="4" title="Vai all pagina: Come raggiungerci" href="/come_raggiungerci/come_raggiungerci.php">Come raggiungerci</a></li>
+						<li id="contatti_v"><a accesskey="P" tabindex="5" title="Vai all pagina: Contatti" href="/contatti/contatti.php">Contatti</a></li>
+						<li id="siti_partner_v"><a accesskey="Q" tabindex="6" title="Vai all pagina: Siti partner" href="/siti_partner/siti_partner.php">Siti partner</a></li>
+						<li id="altro_v"><a accesskey="R" tabindex="7" title="Vai all pagina: Altra pagina" href="/altra_pagina/altra_pagina.php">Altra pagina</a></li>		
+					</ul>
+				</div>	
+				<!--<div id="menu_verticale_2">		Menu di navigazione 2 in colonna sinistra
+					<ul id="lista_menu_verticale_2">
+						<li class="fake_item"></li>
+						<li id="home"><a accesskey="H" tabindex="1" title="Vai alla home page" href="index.php">Home</a></li>
+							<ul id="sottoLista_navigazione_2_a">
+								<li id=""><a accesskey="" tabindex="8" title="" href="">Pagina 1_1</a></li>
+								<li id=""><a accesskey="" tabindex="9" title="" href="">Pagina 1_2</a></li>
+								<li id=""><a accesskey="" tabindex="10" title="" href="">Pagina 1_3</a></li>
+							</ul>
+						<li id=""><a accesskey="" tabindex="2" title="" href="">Pagina 2</a></li>
+							<ul id="sottoLista_navigazione_2_b">
+								<li id=""><a accesskey="" tabindex="11" title="" href="">Pagina 2_1</a></li>
+								<li id=""><a accesskey="" tabindex="12" title="" href="">Pagina 2_2</a></li>
+								<li id=""><a accesskey="" tabindex="13" title="" href="">Pagina 2_3</a></li>
+							</ul>
+						<li id=""><a accesskey="" tabindex="3" title="" href="">Pagina 3</a></li>
+							<ul id="sottoLista_navigazione_2_c">
+								<li id=""><a accesskey="" tabindex="14" title="" href="">Pagina 3_1</a></li>
+								<li id=""><a accesskey="" tabindex="15" title="" href="">Pagina 3_2</a></li>
+								<li id=""><a accesskey="" tabindex="16" title="" href="">Pagina 3_3</a></li>
+							</ul>
+						<li id=""><a accesskey="" tabindex="4" title="" href="">Pagina 4</a></li>
+							<ul id="sottoLista_navigazione_2_d">
+								<li id=""><a accesskey="" tabindex="17" title="" href="">Pagina 4_1</a></li>
+								<li id=""><a accesskey="" tabindex="18" title="" href="">Pagina 4_2</a></li>
+								<li id=""><a accesskey="" tabindex="19" title="" href="">Pagina 4_3</a></li>
+							</ul>
+						<li id=""><a accesskey="" tabindex="5" title="" href="">Pagina 5</a></li>
+							<ul id="sottoLista_navigazione_2_e">
+								<li id=""><a accesskey="" tabindex="20" title="" href="">Pagina 5_1</a></li>
+								<li id=""><a accesskey="" tabindex="21" title="" href="">Pagina 5_2</a></li>
+								<li id=""><a accesskey="" tabindex="22" title="" href="">Pagina 5_3</a></li>
+							</ul>
+					</ul>
+				</div>-->
+				<!-- Inclusione form per newsletter -->
+				<?php include('script_newsletter.php'); ?>
+				<?php include('tag_div_newsletter.php'); ?>
+				<!-- Fine form newsletter -->
+			</div>
+			<!-- Fine colonna sinistra -->
